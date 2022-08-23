b1aca4305188c4dc983a64718abfa33977dc8bb6
userrp <109092365+rpecuch@users.noreply.github.com>
2022-08-23 16:15:20 -0400

    added comments to files to map out tasks to be accomplished

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+//understand moment.js
+
+//turn middle boxes grey if time is passed by setting class to past
+
+//turn middle box red if current hour by setting class to present
+
+//turn middle boxes green if in the future by setting class to future
+
+//allow user to create an event by clicking on a textarea
+
+//event saves to local storage when the save button is pressed
